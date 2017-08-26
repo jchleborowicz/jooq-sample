32b1944fe824e5762ed8c5de162640c8cf8e88ae
Jacek Chleborowicz <jacek@cosmose.co>
2017-08-26 07:17:37 +0200

    Initial commit.

---
+++ src/main/java/pl/jch/tests/jooq/jooqtest/JooqTestApplication.java
@@ -0,0 +1,12 @@
+package pl.jch.tests.jooq.jooqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqTestApplication.class, args);
+    }
+}
