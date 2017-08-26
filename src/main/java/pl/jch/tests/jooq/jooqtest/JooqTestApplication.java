1b64e33a9df72f654ba43e20d1fbba98505f4634
Jacek Chleborowicz <jacek@cosmose.co>
2017-08-26 08:14:36 +0200

    Logging sql.

--- src/main/java/pl/jch/tests/jooq/jooqtest/JooqTestApplication.java
+++ src/main/java/pl/jch/tests/jooq/jooqtest/JooqTestApplication.java
@@ -3,12 +3,20 @@ package pl.jch.tests.jooq.jooqtest;
 import org.jooq.DSLContext;
 import org.jooq.Record1;
 import org.jooq.Result;
+import org.jooq.conf.Settings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JooqTestApplication {
+
+    @Bean
+    public Settings settings() {
+        return new Settings()
+                .withRenderFormatted(true);
+    }
 
     public static void main(String[] args) {
         final ApplicationContext applicationContext = SpringApplication.run(JooqTestApplication.class, args);
@@ -20,6 +28,7 @@ public class JooqTestApplication {
 
     private static void doSth(DSLContext dslContext) {
         final Result<Record1<Integer>> fetch = dslContext.selectOne().fetch();
+
         System.out.println("***");
         System.out.println(fetch);
     }
